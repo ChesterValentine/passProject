d88cc2c9c624123723d60c55dd0ce4479463c9d6
Chester Valentine <lguesdon.pro@laposte.net>
2019-03-09 05:55:42 +0100

    Require Faker + Begin Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,19 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Utilisateur
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $utilisateur = new Utilisateur();
+        for ($i = 0; $i < 10; $i++) {
+            
+        }
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+        //$manager->flush();
     }
 }
